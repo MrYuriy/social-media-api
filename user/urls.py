a5c6e6746b3b3a3b3dce37ce6d261c4376ce661b
YuriiBabiuk <118677914+YuriiBabiuk@users.noreply.github.com>
2023-06-28 07:41:07 +0300

    Add logout endpoint

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,18 @@
+
 from django.urls import path
 
-urlpatterns = [
+from user.views import (
+    CreateUserView,
+    CreateTokenView,
+    ManageUserView,
+    LogoutUserView,
+)
 
+urlpatterns = [
+    path("register/", CreateUserView.as_view(), name="create"),
+    path("login/", CreateTokenView.as_view(), name="login"),
+    path("logout/", LogoutUserView.as_view(), name="logout"),
+    path("me/", ManageUserView.as_view(), name="manage"),
 ]
 
 app_name = "user"
--- user/views.py
+++ user/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.settings import api_settings
+from rest_framework.views import APIView
 
-# Create your views here.
+from user.serializers import UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class LogoutUserView(APIView):
+    serializer_class = UserSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def delete(self, request):
+        request.usern.auth_toke.delete()
+        return Response(status=status.HTTP_200_OK)
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
